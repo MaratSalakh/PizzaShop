ba8d257daa9d5c21b67c26ec1ee4c49545bb8152
Marr <marat.salah1313@gmail.com>
2023-11-29 20:38:42 +0100

    change colors and add animation

--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -1,14 +1,15 @@
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
 import Menu from '../components/Menu';
+import styles from './MenuPage.module.css'
 
 const ShopPage = () => {
   return (
-    <>
+    <div className={styles.page}>
       <NavBar></NavBar>
       <Menu></Menu>
       <Footer></Footer>
-    </>
+    </div>
   )
 };
 
