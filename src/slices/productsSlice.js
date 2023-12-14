85f12fe5da18995446ecf216c74f707b6d8035aa
Marr <marat.salah1313@gmail.com>
2023-12-14 15:23:00 +0100

    merge modalSlice and productsSlice

--- src/slices/productsSlice.js
+++ src/slices/productsSlice.js
@@ -2,20 +2,20 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   entities: {
-    pizza1: { id: 'pizza1', count: 0, price: 500, img: './src/assets/1.jpg', name: 'Pepperoni Pizza' },
-    pizza2: { id: 'pizza2', count: 0, price: 500, img: './src/assets/2.jpg', name: 'Margherita Pizza' },
-    pizza3: { id: 'pizza3', count: 0, price: 1500, img: './src/assets/3.jpg', name: 'BBQ Chicken Pizza' },
-    pizza4: { id: 'pizza4', count: 0, price: 120, img: './src/assets/4.jpg', name: 'Cheese Delight Pizza' },
-    pizza5: { id: 'pizza5', count: 0, price: 900, img: './src/assets/5.jpg', name: 'Veggie Paradise Pizza' },
-    pizza6: { id: 'pizza6', count: 0, price: 3432, img: './src/assets/6.jpg', name: 'Hawaiian Dream Pizza' },
-    pizza7: { id: 'pizza7', count: 0, price: 2300, img: './src/assets/7.jpg', name: 'Pepperoni Extravaganza Pizza' },
-    pizza8: { id: 'pizza8', count: 0, price: 1200, img: './src/assets/8.jpg', name: 'Margherita Delight Pizza' },
-    pizza9: { id: 'pizza9', count: 0, price: 1000, img: './src/assets/1.jpg', name: 'BBQ Chicken Paradise Pizza' },
-    pizza10: { id: 'pizza10', count: 0, price: 900, img: './src/assets/2.jpg', name: 'Cheese Dream Pizza' },
-    pizza11: { id: 'pizza11', count: 0, price: 800, img: './src/assets/3.jpg', name: 'Veggie Extravaganza Pizza' },
-    pizza12: { id: 'pizza12', count: 0, price: 700, img: './src/assets/4.jpg', name: 'Hawaiian Delight Pizza' },
-    pizza13: { id: 'pizza13', count: 0, price: 600, img: './src/assets/5.jpg', name: 'Pepperoni Dream Pizza' },
-    pizza14: { id: 'pizza14', count: 0, price: 400, img: './src/assets/6.jpg', name: 'Margherita Extravaganza Pizza' },
+    pizza1: { id: 'pizza1', count: 0, price: 500, img: './src/assets/1.jpg', name: 'Pepperoni Pizza', modalIsOpen: false },
+    pizza2: { id: 'pizza2', count: 0, price: 500, img: './src/assets/2.jpg', name: 'Margherita Pizza', modalIsOpen: false },
+    pizza3: { id: 'pizza3', count: 0, price: 1500, img: './src/assets/3.jpg', name: 'BBQ Chicken Pizza', modalIsOpen: false },
+    pizza4: { id: 'pizza4', count: 0, price: 120, img: './src/assets/4.jpg', name: 'Cheese Delight Pizza', modalIsOpen: false },
+    pizza5: { id: 'pizza5', count: 0, price: 900, img: './src/assets/5.jpg', name: 'Veggie Paradise Pizza', modalIsOpen: false },
+    pizza6: { id: 'pizza6', count: 0, price: 3432, img: './src/assets/6.jpg', name: 'Hawaiian Dream Pizza', modalIsOpen: false },
+    pizza7: { id: 'pizza7', count: 0, price: 2300, img: './src/assets/7.jpg', name: 'Pepperoni Extravaganza Pizza', modalIsOpen: false },
+    pizza8: { id: 'pizza8', count: 0, price: 1200, img: './src/assets/8.jpg', name: 'Margherita Delight Pizza', modalIsOpen: false },
+    pizza9: { id: 'pizza9', count: 0, price: 1000, img: './src/assets/1.jpg', name: 'BBQ Chicken Paradise Pizza', modalIsOpen: false },
+    pizza10: { id: 'pizza10', count: 0, price: 900, img: './src/assets/2.jpg', name: 'Cheese Dream Pizza', modalIsOpen: false },
+    pizza11: { id: 'pizza11', count: 0, price: 800, img: './src/assets/3.jpg', name: 'Veggie Extravaganza Pizza', modalIsOpen: false },
+    pizza12: { id: 'pizza12', count: 0, price: 700, img: './src/assets/4.jpg', name: 'Hawaiian Delight Pizza', modalIsOpen: false },
+    pizza13: { id: 'pizza13', count: 0, price: 600, img: './src/assets/5.jpg', name: 'Pepperoni Dream Pizza', modalIsOpen: false },
+    pizza14: { id: 'pizza14', count: 0, price: 400, img: './src/assets/6.jpg', name: 'Margherita Extravaganza Pizza', modalIsOpen: false },
   },
   ids: ['pizza1', 'pizza2', 'pizza3', 'pizza4', 'pizza5', 'pizza6', 'pizza7', 'pizza8', 'pizza9', 'pizza10', 'pizza11', 'pizza12', 'pizza13', 'pizza14',],
 };
@@ -33,9 +33,17 @@ const productsSlice = createSlice({
     resetCount: (state, action) => {
       state.entities[action.payload].count = 0;
     },
+    openModal: (state, action) => {
+      document.body.classList.add('scrollLock');
+      state.entities[action.payload].modalIsOpen = true;
+    },
+    closeModal: (state, action) => {
+      document.body.classList.remove('scrollLock');
+      state.entities[action.payload].modalIsOpen = true;
+    }
   },
 });
 
-export const { countPlus, countMinus, resetCount } = productsSlice.actions;
+export const { countPlus, countMinus, resetCount, openModal, closeModal } = productsSlice.actions;
 
 export default productsSlice.reducer;
--- src/slices/store.js
+++ src/slices/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productsReducer from './productsSlice.js'
-import modalReducer from './modalSlice.js'
 
 export default configureStore({
   reducer: {
     products: productsReducer,
-    modalState: modalReducer,
   },
 });
