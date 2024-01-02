e9caef94538c17347d99ffb0d974cd22148b5b12
Marr <marat.salah1313@gmail.com>
2024-01-02 18:46:02 +0100

    add size button in modal

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -1,11 +1,16 @@
-import styles from './CardProduct.module.css'
-import PropTypes from 'prop-types'
+import styles from "./CardProduct.module.css";
+import PropTypes from "prop-types";
 // import cn from 'classnames';
 
-import { ModalCard } from './ModalCard';
+import { ModalCard } from "./ModalCard";
 
-import { openModal, countMinus, countPlus, resetCount } from '../slices/productsSlice';
-import { useDispatch } from 'react-redux';
+import {
+  openModal,
+  countMinus,
+  countPlus,
+  resetCount,
+} from "../slices/productsSlice";
+import { useDispatch } from "react-redux";
 
 const CardProduct = (props) => {
   const { price, img, name, id, count, description } = props.product;
@@ -15,12 +20,23 @@ const CardProduct = (props) => {
 
   const menuCard = (
     <div className={styles.card}>
-      <img className={styles.img} onClick={() => dispatch(openModal(id))} src={img} alt="pizza" width={300} height={300} />
+      <img
+        className={styles.img}
+        onClick={() => dispatch(openModal(id))}
+        src={img}
+        alt="pizza"
+        width={300}
+        height={300}
+      />
       <h2 onClick={() => dispatch(openModal(id))}>{name}</h2>
       <span className={styles.description}>{description}</span>
       <div className={styles.cardFooter}>
         <span className={styles.cost}>from {price} $</span>
-        <button className={styles.button} onClick={() => dispatch(openModal(id))}>Choose</button>
+        <button
+          className={styles.button}
+          onClick={() => dispatch(openModal(id))}>
+          Choose
+        </button>
       </div>
       <ModalCard product={props.product}></ModalCard>
     </div>
@@ -28,33 +44,55 @@ const CardProduct = (props) => {
 
   const cartCard = (
     <div className={styles.card}>
-      <img className={styles.img} src={img} alt="pizza" width={300} height={300} />
+      <img
+        className={styles.img}
+        src={img}
+        alt="pizza"
+        width={300}
+        height={300}
+      />
       <h2>{name}</h2>
       <span className={styles.description}>Delicious pie with cherry</span>
       <div className={styles.cardFooterBasket}>
-
         <span className={styles.cost}>{price * count} $</span>
-        <button className={styles.button} onClick={() => dispatch(openModal(id))}>Change</button>
+        <button
+          className={styles.button}
+          onClick={() => dispatch(openModal(id))}>
+          Change
+        </button>
 
         <div className={styles.buttonGroupBasket}>
-          <button className={styles.leftButton} type='button' onClick={() => dispatch(countMinus(id))}>-</button>
+          <button
+            className={styles.leftButton}
+            type="button"
+            onClick={() => dispatch(countMinus(id))}>
+            -
+          </button>
           <button className={styles.counter}>{count}</button>
-          <button className={styles.rightButton} type='button' onClick={() => dispatch(countPlus(id))}>+</button>
+          <button
+            className={styles.rightButton}
+            type="button"
+            onClick={() => dispatch(countPlus(id))}>
+            +
+          </button>
         </div>
 
-        <button className={styles.button} onClick={() => dispatch(resetCount(id))}>X</button>
-
+        <button
+          className={styles.button}
+          onClick={() => dispatch(resetCount(id))}>
+          X
+        </button>
       </div>
       <ModalCard product={props.product}></ModalCard>
     </div>
   );
 
-  return (cardType === 'menu' ? menuCard : cartCard);
+  return cardType === "menu" ? menuCard : cartCard;
 };
 
 CardProduct.propTypes = {
   product: PropTypes.object,
   cardType: PropTypes.string,
-}
+};
 
 export default CardProduct;
--- src/components/ModalCard.jsx
+++ src/components/ModalCard.jsx
@@ -1,41 +1,62 @@
-import styles from './ModalCard.module.css'
-import cn from 'classnames'
-import { useDispatch } from 'react-redux'
-import { closeModal } from '../slices/productsSlice'
-import { countPlus } from '../slices/productsSlice'
-import PropTypes from 'prop-types'
+import styles from "./ModalCard.module.css";
+import cn from "classnames";
+import { useDispatch } from "react-redux";
+import { closeModal } from "../slices/productsSlice";
+import { countPlus } from "../slices/productsSlice";
+import PropTypes from "prop-types";
 
 export const ModalCard = (props) => {
   const { id, img, name, modalIsOpen, description } = props.product;
 
   const dispatch = useDispatch();
 
-  const showHideClassName = modalIsOpen ?
-    cn(styles.modal, styles.displayBlock) : cn(styles.modal, styles.displayNone);
+  const showHideClassName = modalIsOpen
+    ? cn(styles.modal, styles.displayBlock)
+    : cn(styles.modal, styles.displayNone);
 
   return (
     <div onClick={() => dispatch(closeModal(id))} className={showHideClassName}>
-      <section onClick={(e) => e.stopPropagation()} className={styles.modalMain}>
+      <section
+        onClick={(e) => e.stopPropagation()}
+        className={styles.modalMain}>
         <div className={styles.innerContent}>
-          <img className={styles.img} src={img} alt="pizza" height={300} width={300} />
+          <img
+            className={styles.img}
+            src={img}
+            alt="pizza"
+            height={300}
+            width={300}
+          />
           <div className={styles.pizzaSettings}>
-            <div className={styles.header}>
-              <h1>{name}</h1>
-            </div>
+            <h1>{name}</h1>
             <span className={styles.description}>{description}</span>
-            <button className={cn(styles.button, styles.addButton)} type='button' onClick={() => {
-              dispatch(countPlus(id));
-              dispatch(closeModal(id));
-            }}>Add in cart</button>
+            <div className={styles.buttonGroup}>
+              <button className={styles.leftButton} type="button">
+                Small
+              </button>
+              <button className={styles.counter}>Medium</button>
+              <button className={styles.rightButton} type="button">
+                Big
+              </button>
+            </div>
+            <button
+              className={cn(styles.button, styles.addButton)}
+              type="button"
+              onClick={() => {
+                dispatch(countPlus(id));
+                dispatch(closeModal(id));
+              }}>
+              Add in cart
+            </button>
           </div>
         </div>
-      </section >
-    </div >
+      </section>
+    </div>
   );
 };
 
 ModalCard.propTypes = {
   product: PropTypes.object,
   id: PropTypes.string,
-  count: PropTypes.number
-}
+  count: PropTypes.number,
+};
